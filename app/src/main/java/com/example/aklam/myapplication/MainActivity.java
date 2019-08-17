dd944f576d5f6440111cac3241e0c46e6f3b5c15
Agnaramon Boris-Carnot <agnaramonboris@gmail.com>
2019-08-17 17:13:15 +0000

    suggestion revision de code

--- app/src/main/java/com/example/aklam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aklam/myapplication/MainActivity.java
@@ -11,52 +11,63 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et3;
-    EditText et4;
-    Button b;
-    TextView tv;
+    EditText editPremiereNombre;
+    EditText editSecondNombre;
+    Button buttonCalculer;
+    TextView textResultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b =  (Button) findViewById(R.id.button);
-        et3 = (EditText) findViewById(R.id.editText3);
-        et4 = (EditText) findViewById(R.id.editText4);
-        tv = (TextView ) findViewById(R.id.textView5);
+        buttonCalculer = (Button) findViewById(R.id.buttonCalculer);
+        editPremiereNombre = (EditText) findViewById(R.id.editPremiereNombre);
+        editSecondNombre = (EditText) findViewById(R.id.editSecondNombre);
+        textResultat = (TextView) findViewById(R.id.textResultat);
 
-
-
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (et3.getText().toString().isEmpty() || et4.getText().toString().isEmpty())
-                {
-                    Toast.makeText(MainActivity.this, "Remplissez obligatoirement les deux champs svp", Toast.LENGTH_LONG).show();
-                }else {
-                    //Toast.makeText(MainActivity.this, R.string.Erreur, Toast.LENGTH_SHORT).show();
-                    int num1 = Integer.parseInt(et3.getText().toString());
-                    int num2 = Integer.parseInt(et4.getText().toString());
-
-                    int som = num1 + num2;
-
-                    tv.setText(String.valueOf(som));
-                }
-                /*
-                if (et3.getText().toString().length() == 0) {
-                    et3.setText("0");
-                }
-                if (et4.getText().toString().length() == 0) {
-                    et4.setText("0");
-                }
-                */
-
-
-
-                };
-        });
+        buttonCalculer.setOnClickListener(calculerListener);
 
     }
+
+    View.OnClickListener calculerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(verifierNombreSaisie()){
+                calculerNombre();
+            }
+        }
+    };
+
+    void calculerNombre(){
+
+        int nombre1 = Integer.parseInt(editPremiereNombre.getText().toString());
+        int nombre2 = Integer.parseInt(editSecondNombre.getText().toString());
+
+        int somme = nombre1 + nombre2;
+
+        textResultat.setText(String.valueOf(somme));
+    }
+
+    boolean verifierNombreSaisie(){
+
+        if(editPremiereNombre.getText().toString().isEmpty()){
+            Toast.makeText(this, "Le nombre 1 est obligatoire", Toast.LENGTH_LONG).show();
+
+            // Place le curseur sur la zone de saisie du nombre 1
+            editPremiereNombre.requestFocus();
+            return false;
+        }
+
+        if(editSecondNombre.getText().toString().isEmpty()){
+            Toast.makeText(this, "Le nombre 1 est obligatoire", Toast.LENGTH_LONG).show();
+
+            // Place le curseur sur la zone de saisie du nombre 2
+            editSecondNombre.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
 }
